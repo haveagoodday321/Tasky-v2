79f2b95759a469817982da1d795ad4f949de09a3
haveagoodday321 <sitholesibusiso4132@gmail.com>
2026-07-17 23:57:11 +0200

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,1 +1,44 @@
+/* ============================
+   theme.js
+============================ */
 
+function toggleTheme() {
+
+    document.body.classList.toggle("light");
+
+    const mode =
+        document.body.classList.contains("light")
+        ? "light"
+        : "dark";
+
+    save("theme", mode);
+
+}
+
+function loadTheme() {
+
+    const theme =
+        load("theme", "dark");
+
+    if (theme === "light") {
+
+        document.body.classList.add("light");
+
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    loadTheme();
+
+    const btn =
+        document.getElementById("themeBtn");
+
+    if (btn) {
+
+        btn.addEventListener("click", toggleTheme);
+
+    }
+
+});
