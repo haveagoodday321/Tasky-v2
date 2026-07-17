ee2bdadb22146e998025ecbead52fc272548fe74
haveagoodday321 <sitholesibusiso4132@gmail.com>
2026-07-17 23:56:40 +0200

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,39 @@
+/* ============================
+   storage.js
+============================ */
 
+function save(key, value) {
+    localStorage.setItem(
+        key,
+        JSON.stringify(value)
+    );
+}
+
+function load(key, defaultValue = []) {
+
+    const data = localStorage.getItem(key);
+
+    if (data === null) {
+        return defaultValue;
+    }
+
+    try {
+        return JSON.parse(data);
+    } catch (e) {
+        console.error("Storage Error:", e);
+        return defaultValue;
+    }
+
+}
+
+function remove(key) {
+
+    localStorage.removeItem(key);
+
+}
+
+function clearStorage() {
+
+    localStorage.clear();
+
+}
