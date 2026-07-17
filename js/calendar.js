e4ea824fb94871c87adc0a216d224b815dbcb4aa
haveagoodday321 <sitholesibusiso4132@gmail.com>
2026-07-17 23:57:44 +0200

    Update calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -1,1 +1,50 @@
+/* ============================
+   calendar.js
+============================ */
 
+function buildCalendar() {
+
+    const calendar =
+        document.getElementById("calendar");
+
+    if (!calendar) return;
+
+    calendar.innerHTML = "";
+
+    const today = new Date();
+
+    const days =
+        new Date(
+            today.getFullYear(),
+            today.getMonth() + 1,
+            0
+        ).getDate();
+
+    document.getElementById(
+        "calendarMonth"
+    ).textContent =
+        today.toLocaleString("default", {
+            month: "long",
+            year: "numeric"
+        });
+
+    for (let i = 1; i <= days; i++) {
+
+        const day =
+            document.createElement("div");
+
+        day.className = "day";
+
+        if (i === today.getDate()) {
+
+            day.classList.add("today");
+
+        }
+
+        day.textContent = i;
+
+        calendar.appendChild(day);
+
+    }
+
+}
