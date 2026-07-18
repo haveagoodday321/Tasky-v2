0f4439c2d5713538166806c8259fe3fa305d534f
haveagoodday321 <sitholesibusiso4132@gmail.com>
2026-07-18 23:45:20 +0200

    Update achievements.js

--- js/achievements.js
+++ js/achievements.js
@@ -1,1 +1,30 @@
+/* ===============================
+   achievements.js
+=============================== */
 
+const achievements = [
+
+{
+title:"First Task",
+goal:1
+},
+
+{
+title:"Task Master",
+goal:25
+},
+
+{
+title:"Productivity Pro",
+goal:100
+}
+
+];
+
+function updateAchievements(){
+
+console.log(
+"Achievements Updated"
+);
+
+}
