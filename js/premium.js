998b3ee1abbff88649893793e8e20f10220ce79b
haveagoodday321 <sitholesibusiso4132@gmail.com>
2026-07-18 23:46:57 +0200

    Update premium.js

--- js/premium.js
+++ js/premium.js
@@ -1,1 +1,15 @@
+/* ===============================
+   premium.js
+=============================== */
 
+let premium = false;
+
+function activatePremium(){
+
+premium = true;
+
+alert(
+"Tasky Premium Activated!"
+);
+
+}
